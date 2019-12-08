2686bfc9ea9477ed23ed1ae837dd9a298fa5cf11
Yarob Al Mostafa <yarob.hm@gmail.com>
2019-12-08 14:33:00 +0300

    some thing

--- removeBranchOnPR.js
+++ removeBranchOnPR.js
@@ -2,15 +2,16 @@ const core = require('@actions/core');
 const github = require('@actions/github')
 
 try{
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}`);
+
 	const time = (new Date).toTimeString();
 	core.setOutput("time", time);
 
 	// Get the JSON webhook payload for the event that triggered the workflow
   	const payload = JSON.stringify(github.context.payload, undefined, 2)
+  	const jsonPayload = github.context.payload
   	console.log("//////////////////////////////////")
-  	console.log(`The event payload: ${payload}`);
+  	console.log("The json pay load is:");
+  	console.log(jsonPayload)
   	console.log("//////////////////////////////////")
 
 }catch (error){
